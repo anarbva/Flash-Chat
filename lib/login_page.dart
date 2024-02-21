b57a3e11130bced737b58e981bfce196d63e62c5
Aidanek <anarbaevaaidanek22@gmail.com>
2024-02-21 21:27:06 +0400

    changes to all file

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,7 +6,6 @@ class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
-
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
